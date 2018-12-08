5edf5f32c8bb16e6e2f02eaef9c3f67f6c9cac7f
tinghao.ma <tinghao.ma@ele.me>
2018-12-08 11:16:00 +0800

    page 样式

--- pages/mth/mth.js
+++ pages/mth/mth.js
@@ -16,17 +16,24 @@ Page({
         ],
         list:['1', '2', '3', '4'],
     },
+    customData: {
+        hi: 'alibaba'
+    },
     onLoad() {
         console.log('onLoad');
         this.setData({
-            title: 'Taobao--' + app.appInfo
+            title: 'Taobao--' + app.appInfo,
+            noDefined:'该值没有定义 直接setData里面设置'
         })
     },
     onBtnClicked(e) {
         console.log('onBtnClicked --' + e);
         this.setData({
-            title: 'AliBaba',
-        });
+            name: 'AliBaba',
+        },() => { // 接受传递回调函数
+            console.log(this) // this 当前页面实例
+            this.setData({ name: this.data.name + ', ' + 'welcome!'})
+          });
     },
     bringToFront(e){
         console.log(e)
