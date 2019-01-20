d1d6269ad91ba93383083cc15f4768c9ccad5e93
tinghao.ma <tinghao.ma@ele.me>
2019-01-20 20:35:00 +0800

    调整布局

--- pages/api/api_demo.js
+++ pages/api/api_demo.js
@@ -5,11 +5,30 @@ Page({
 
     },
     onLoad(query) {
-        console.log(app)
         const { navigationBarHeight, statusBarHeight } = app.getTitleBarHeights();
         app.getTitleBarHeights().then(({ navigationBarHeight, statusBarHeight }) => {
             // app.toast(`navigationBarHeight =${navigationBarHeight}  statusBarHeight=${statusBarHeight}`)
+            const screenWidth = app.screenWidth;
+            const screenHeight = app.screenHeight;
+            const stikyHeaderHeight =
+                (750 / screenWidth) * (navigationBarHeight + statusBarHeight);
+            const menuHeight =
+                (750 / screenWidth) * screenHeight - stikyHeaderHeight;
+            app.toast(menuHeight)
+            const titleBarHeight = navigationBarHeight + statusBarHeight;
+            this.setData({
+                titleBarHeight,
+                menuHeight,
+            })
         })
     },
+    onScroll() {
 
+    },
+    onScrollToUpper() {
+
+    },
+    onScrollToLower() {
+        app.toast('onScrollToLoweronScrollToLower')
+    }
 })
